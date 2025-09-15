2b9f07b31081d3491d19b52968d6d5efa11349b2
thewisepeter <rempeterwisdom@gmail.com>
2025-09-15 14:13:39 +0300

    Add simple-git and zod dependencies; implement code review tool

--- index.ts
+++ index.ts
@@ -1,11 +1,26 @@
-import { generateText } from "ai";
-// import the google module from the ai-sdk package
+import { stepCountIs, streamText } from "ai";
 import { google } from "@ai-sdk/google";
+import { SYSTEM_PROMPT } from "./prompts";
+import { getFileChangesInDirectoryTool } from "./tools";
 
-// Specify the model to use for generating text and a prompt
-const { text } = await generateText({
-  model: google("models/gemini-2.5-flash"),
-  prompt: "What is an AI agent?",
-});
+const codeReviewAgent = async (prompt: string) => {
+  const result = streamText({
+    model: google("models/gemini-2.5-flash"),
+    prompt,
+    system: SYSTEM_PROMPT,
+    tools: {
+      getFileChangesInDirectoryTool: getFileChangesInDirectoryTool,
+    },
+    stopWhen: stepCountIs(10),
+  });
 
-console.log(text);
+  for await (const chunk of result.textStream) {
+    process.stdout.write(chunk);
+  }
+};
+
+// Specify which directory the code review agent should review changes in your prompt
+
+await codeReviewAgent(
+  "Review the code changes in '../agent_wisdom' directory, make your reviews and suggestions file by file",
+);
